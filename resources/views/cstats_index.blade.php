96d2e15f3091242c0c81892e689131e76e05d141
Colin M <colcmorris@gmail.com>
2021-08-01 18:23:01 +0100

    Add more covid stats sbadmin data and check if looks ok on live

--- resources/views/cstats_index.blade.php
+++ resources/views/cstats_index.blade.php
@@ -453,7 +453,7 @@ $bodyid = "page-top";
                                 <!-- Card Header - Dropdown -->
                                 <div
                                     class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
-                                    <h6 class="m-0 font-weight-bold text-primary">Earnings Overview</h6>
+                                    <h6 class="m-0 font-weight-bold text-primary">Covid Cases in the UK</h6>
                                     <div class="dropdown no-arrow">
                                         <a class="dropdown-toggle" href="#" role="button" id="dropdownMenuLink"
                                             data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
@@ -472,12 +472,43 @@ $bodyid = "page-top";
                                 <!-- Card Body -->
                                 <div class="card-body">
                                     <div class="chart-area">
-                                        <canvas id="myAreaChart"></canvas>
+                                       <?php // <canvas id="myAreaChart"></canvas> ?>
+                                       <canvas id="covidCasesChart"></canvas>
                                     </div>
                                 </div>
+                             
                             </div>
                         </div>
+                       <div class="col-xl-8 col-lg-7">
+                            <div class="card shadow mb-4">
+                                <!-- Card Header - Dropdown -->
+                                <div
+                                    class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
+                                    <h6 class="m-0 font-weight-bold text-primary">Covid Deaths in the UK</h6>
+                                    <div class="dropdown no-arrow">
+                                        <a class="dropdown-toggle" href="#" role="button" id="dropdownMenuLink"
+                                            data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                            <i class="fas fa-ellipsis-v fa-sm fa-fw text-gray-400"></i>
+                                        </a>
+                                        <div class="dropdown-menu dropdown-menu-right shadow animated--fade-in"
+                                            aria-labelledby="dropdownMenuLink">
+                                            <div class="dropdown-header">Dropdown Header:</div>
+                                            <a class="dropdown-item" href="#">Action</a>
+                                            <a class="dropdown-item" href="#">Another action</a>
+                                            <div class="dropdown-divider"></div>
+                                            <a class="dropdown-item" href="#">Something else here</a>
+                                        </div>
+                                    </div>
+                                </div>
 
+                                <!-- Card Body -->
+                                <div class="card-body">
+                                    <div class="chart-area">
+                                       <canvas id="covidDeathsChart"></canvas>
+                                    </div>
+                                </div>                                
+                            </div>
+                        </div>
                         <!-- Pie Chart -->
                         <div class="col-xl-4 col-lg-5">
                             <div class="card shadow mb-4">
@@ -757,31 +788,125 @@ document.addEventListener('DOMContentLoaded', function () {
                     $("#total_deaths_to_date").html(deaths_to_date);
                     $("#total_deaths_to_date_date").html(to_date_date);
                }
+               caseslabels = new Array(result.data.length);
+               casesdata = new Array(result.data.length);
+               deathsdata = new Array(result.data.length);
+               for(var i=0; i < result.data.length; i++)
+               {
+                   caseslabels[i] = result.data[i].date;
+                   casesdata[i] = result.data[i].cases;
+                   deathsdata[i] = result.data[i].expired;
+               }
+               //var caseslabels =  ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+               //var casesdata = [0, 10000, 5000, 15000, 10000, 20000, 15000, 25000, 20000, 30000, 25000, 40000];
+               
+               covidChartData(caseslabels, casesdata, "covidCasesChart");
+               covidChartData(caseslabels, deathsdata, "covidDeathsChart");
         }, "json");
-  /* 
-      $.ajax({url: "/cvstats", dataType:"json", success: function(result){ // {"date_from":"2020-02-01", "date_to": "2021-07-30"}
-         console.log("== DEBUG AJAX RESULTS ==");
-         console.log(result);
-         console.log(result.data[0].date);
-         
-        if(document.getElementById("total_cases_to_date")!== null)
-        {         
-             const cases_to_date = result.data[result.data.length -1].cases;
-             const deaths_to_date = result.data[result.data.length -1].expired;
-             const to_date_date = " (" + result.data[result.data.length -1].date + ")";
-             console.log(cases_to_date);
-             $("#total_cases_to_date").html(cases_to_date);
-             $("#total_cases_to_date_date").html(to_date_date);
-             $("#total_deaths_to_date").html(deaths_to_date);
-             $("#total_deaths_to_date_date").html(to_date_date);
-        }
-            //$("#covid_data").html($("#covid_data").html() + result);
-        }});
-         * 
-   */
+
+
+
 });
 
-    
+
+function covidChartData(chartlabels, chartdata, chartname)
+{
+    //const chartname = "covidCasesChart"; // ** Edit by colin
+    if(document.getElementById(chartname)!== null)
+    {
+        var ctx = document.getElementById(chartname);
+        var myLineChart = new Chart(ctx, {
+          type: 'line',
+          data: {
+            labels: chartlabels,
+            datasets: [{
+              label: "Case dates",
+              lineTension: 0.3,
+              backgroundColor: "rgba(78, 115, 223, 0.05)",
+              borderColor: "rgba(78, 115, 223, 1)",
+              pointRadius: 3,
+              pointBackgroundColor: "rgba(78, 115, 223, 1)",
+              pointBorderColor: "rgba(78, 115, 223, 1)",
+              pointHoverRadius: 3,
+              pointHoverBackgroundColor: "rgba(78, 115, 223, 1)",
+              pointHoverBorderColor: "rgba(78, 115, 223, 1)",
+              pointHitRadius: 10,
+              pointBorderWidth: 2,
+              data: chartdata,
+            }],
+
+          },
+          options: {
+            maintainAspectRatio: false,
+            layout: {
+              padding: {
+                left: 10,
+                right: 25,
+                top: 25,
+                bottom: 0
+              }
+            },
+            scales: {
+              xAxes: [{
+                time: {
+                  unit: 'date'
+                },
+                gridLines: {
+                  display: false,
+                  drawBorder: false
+                },
+                ticks: {
+                  maxTicksLimit: 7
+                }
+              }],
+              yAxes: [{
+                ticks: {
+                  maxTicksLimit: 5,
+                  padding: 10,
+                  // Include a dollar sign in the ticks
+                  callback: function(value, index, values) {
+                    //return '$' + number_format(value);
+                    return value;
+                  }
+                },
+                gridLines: {
+                  color: "rgb(234, 236, 244)",
+                  zeroLineColor: "rgb(234, 236, 244)",
+                  drawBorder: false,
+                  borderDash: [2],
+                  zeroLineBorderDash: [2]
+                }
+              }],
+            },
+            legend: {
+              display: false
+            },
+            tooltips: {
+              backgroundColor: "rgb(255,255,255)",
+              bodyFontColor: "#858796",
+              titleMarginBottom: 10,
+              titleFontColor: '#6e707e',
+              titleFontSize: 14,
+              borderColor: '#dddfeb',
+              borderWidth: 1,
+              xPadding: 15,
+              yPadding: 15,
+              displayColors: false,
+              intersect: false,
+              mode: 'index',
+              caretPadding: 10,
+              callbacks: {
+                label: function(tooltipItem, chart) {
+                  var datasetLabel = chart.datasets[tooltipItem.datasetIndex].label || '';
+                  //return datasetLabel + ': $' + number_format(tooltipItem.yLabel);
+                  return datasetLabel + ': ' + number_format(tooltipItem.yLabel);
+                }
+              }
+            }
+          }
+        });    
+    }    
+}    
     
     </script>
 @endsection
