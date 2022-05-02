6e5b1566a36d116752bf20ef94c6a952eff10063
Colin M <colcmorris@gmail.com>
2022-05-02 17:36:57 +0100

    First working draft of new index page

--- app/Http/Controllers/Showcase.php
+++ app/Http/Controllers/Showcase.php
@@ -19,7 +19,7 @@ class Showcase extends Controller
     {
         // Add menu items for index page. [ ->indexItem(string $image, string $route, string $description)]
         $apps = [];
-        $apps[] = $this->indexItem("covid_1_225h.jpg", "cstats_index","UK Covid infection rates and mortality rates updated on a schedule during weekdays", " offset-md-2 col-md-5");
+        $apps[] = $this->indexItem("covid_2_225h.jpg", "cstats_index","UK Covid infection rates and mortality rates updated on a schedule during weekdays", " offset-md-2 col-md-5");
         $apps[] = $this->indexItem("weather_1_225h.jpg", "weatherindex","Weather forecasts for various cities in the UK", " col-md-5");
         //$apps[] = $this->indexItem("", "weatherindex","Content coming soon");
 
--- resources/views/album/index.blade.php
+++ resources/views/album/index.blade.php
@@ -51,16 +51,18 @@ $cols_md = isset($cols_md)? " col-md-". $cols_md ." ": " col-md-5 ";
   <div class="collapse bg-dark" id="navbarHeader">
     <div class="container">
       <div class="row">
-        <div class="col-sm-8 col-md-7 py-4">
-          <h4 class="text-white">About</h4>
-          <p class="text-muted">Add some information about the album below, the author, or any other background context. Make it a few sentences long so folks can pick up some informative tidbits. Then, link them off to some social networking sites or contact information.</p>
+        <div class="text-white col-sm-8 col-md-7 py-4">
+        <h4>This is a website that showcases Colin's skills in web design.</h4><p>Technologies used include JavaScript, PHP, React.js, HTML, CSS and JQuery</p>
+        <p>See the code repository for the apps on this site at the following location:<br>
+        <a href="https://github.com/colinogreen/sho-tech">Colins Showcase code on Github</a></p>
         </div>
         <div class="col-sm-4 offset-md-1 py-4">
           <h4 class="text-white">Contact</h4>
-          <ul class="list-unstyled">
-            <li><a href="#" class="text-white">Follow on Twitter</a></li>
+          <ul class="text-white list-unstyled">
+            <li>Contacts to be confirmed...</li>
+           <?php /* <li><a href="#" class="text-white">Follow on Twitter</a></li>
             <li><a href="#" class="text-white">Like on Facebook</a></li>
-            <li><a href="#" class="text-white">Email me</a></li>
+            <li><a href="#" class="text-white">Email me</a></li> */ ?>
           </ul>
         </div>
       </div>
@@ -73,11 +75,12 @@ $cols_md = isset($cols_md)? " col-md-". $cols_md ." ": " col-md-5 ";
         &#160;<strong>Technohelp</strong>
 
       </a>
-      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarHeader" aria-controls="navbarHeader" aria-expanded="false" aria-label="Toggle navigation">
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarHeader" aria-controls="navbarHeader" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
     </div>
   </div>
+    
 </header>
 
 <main>
@@ -134,8 +137,8 @@ $cols_md = isset($cols_md)? " col-md-". $cols_md ." ": " col-md-5 ";
     <p class="float-end mb-1">
       <a href="#">Back to top</a>
     </p>
-    <p class="mb-1">Album example is &copy; Bootstrap, but please download and customize it for yourself!</p>
-    <p class="mb-0">New to Bootstrap? <a href="/">Visit the homepage</a> or read our <a href="../getting-started/introduction/">getting started guide</a>.</p>
+    <p class="mb-1">Expand the menu at the top right of the screen for further information on this web site!</p>
+
   </div>
 </footer>
 
